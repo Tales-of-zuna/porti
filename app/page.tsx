5fa6bc88c634089a549551b6b54f816f392c9fc3
Zuunbileg <zuunbileg.b@unitel.mn>
2023-08-24 17:47:32 +0800

    ffff

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ const Home = () => {
               : "opacity-0"
           }`}
           height={500}
-          width={1080}
+          width={window.innerWidth}
           objectFit={"contain"}
           alt=""
           src="https://res.cloudinary.com/dwl0svlrm/image/upload/v1692865826/background_tmp8bb.gif"
