4ad109a6e9c841dde31e6ce213cd3cf436b3c39b
Zuunbileg <zuunbileg.b@unitel.mn>
2023-08-24 09:14:31 +0800

    init

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="w-screen fixed p-4 shadow-xl backdrop-blur-lg bg-slate-800 bg-opacity-40">
+      Hello
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ models/userModel.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//Export the model
+module.exports = model("User", userSchema);
